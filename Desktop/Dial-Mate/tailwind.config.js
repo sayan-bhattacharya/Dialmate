4d2b2170461f85f74cf989389bb0903a221ed53a
basel-abdelazim <basel.abdelazim@gmail.com>
2024-10-17 16:57:19 +0200

    Added new feature or changes

---
+++ Desktop/Dial-Mate/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+import daisyui from 'daisyui';
+
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
